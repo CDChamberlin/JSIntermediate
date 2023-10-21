8bb1e4ce6504630ab5a9d0e770cfc0dbc9a3c71a
Collin <cdchamberlin.rva@gmail.com>
2023-10-21 16:05:55 -0400

    Forgot part b of question. Added it now

--- index.js
+++ index.js
@@ -16,4 +16,6 @@ function truncate(str, max){
     if (str.length > max) return str.substring(0, max) +'...'
     else return str
 }
+truncate2 = (str, max) => str.length > max ?  str.substring(0, max) + '...' : str
 console.log(truncate('This text will be truncated if it is too long', 25))
+console.log(truncate2('This text will be truncated if it is too long', 25))
